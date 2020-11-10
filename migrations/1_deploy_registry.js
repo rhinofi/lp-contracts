e91c973e6a158d208d117cdd986c9a7d84b25db8
Will <will.harborne@googlemail.com>
2020-11-10 20:10:23 +0000

    Make oracle variables internal

--- migrations/1_deploy_registry.js
+++ migrations/1_deploy_registry.js
@@ -3,7 +3,9 @@ const { deployProxy } = require('@openzeppelin/truffle-upgrades')
 const MasterTransferRegistry = artifacts.require('MasterTransferRegistry')
 
 module.exports = async function (deployer) {
-  // Rinkeby
+  if (deployer.network !== 'rinkeby') {
+    return
+  }
   const uniswapFactory = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
   const wethAddress = '0xc778417E063141139Fce010982780140Aa0cD5Ab'
   const nectarAddress = '0x5dc480718DEBF5ED304C835EB14448A4810c06A6'
